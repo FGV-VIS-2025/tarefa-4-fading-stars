6a888df875609af58ad940dbf339cd581acbc530
pedrotokar <pedrotokar2004@gmail.com>
2025-04-23 13:04:50 -0300

    Add initial layout (to be improved)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div class = "all">
+    <div class = "filters">
+        <h2>Aqui entram os filtros</h2>
+    </div>
+
+    <div class = "viz">
+        <h2>Aqui entra a viz</h2>
+    </div>
+</div>
+
+<style>
+.all {
+    display: grid;
+	grid-template-columns : 40% 45%;
+}
+</style>
