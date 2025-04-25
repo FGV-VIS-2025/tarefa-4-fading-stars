1671646e87e4006d1141074e0960999180c81121
vitor do nascimento <vitornascimento@proton.me>
2025-04-25 17:03:11 -0300

    add stars data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,42 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import * as d3 from "d3";
 
-<div class = "all">
-    <div class = "filters">
-        <h2>Aqui entram os filtros</h2>
-    </div>
+	import stars from "$lib/data/stars-data.json";
 
-    <div class = "viz">
-        <h2>Aqui entra a viz</h2>
-    </div>
+	let width = 600;
+	let height = 600;
+
+	$: xScale = d3.scaleLinear().range([0, width]);
+
+	$: yScale = d3.scaleLinear().range([0, height]);
+</script>
+
+<div class="all">
+	<div class="filters">
+		<h2>Aqui entram os filtros</h2>
+	</div>
+
+	<div class="viz">
+		<h2>Aqui entra a viz</h2>
+	</div>
 </div>
 
+<svg {width} {height} viewBox="-600 -600 {width * 2} {height * 2}">
+	<g class="stars">
+		{#each stars as star}
+			<circle
+				cx={xScale(star.x_proj)}
+				cy={yScale(star.y_proj)}
+				r="1"
+				fill="white"
+			/>
+		{/each}
+	</g>
+</svg>
+
 <style>
-.all {
-    display: grid;
-	grid-template-columns : 40% 45%;
-}
+	.all {
+		display: grid;
+		grid-template-columns: 40% 45%;
+	}
 </style>
