24772bb0918af4733e0b8cc18d8e5ab007adabba
vitor do nascimento <vitornascimento@proton.me>
2025-04-25 17:07:52 -0300

    add stars-plot module

---
+++ src/lib/components/stars-plot.svelte
@@ -0,0 +1,24 @@
+<script>
+	import * as d3 from "d3";
+
+	import stars from "$lib/data/stars-data.json";
+
+	let width = 600;
+	let height = 600;
+
+	$: xScale = d3.scaleLinear().range([0, width]);
+	$: yScale = d3.scaleLinear().range([0, height]);
+</script>
+
+<svg {width} {height} viewBox="-600 -600 {width * 2} {height * 2}">
+	<g class="stars">
+		{#each stars as star}
+			<circle
+				cx={xScale(star.x_proj)}
+				cy={yScale(star.y_proj)}
+				r="1"
+				fill="white"
+			/>
+		{/each}
+	</g>
+</svg>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,5 @@
 <script>
-	import * as d3 from "d3";
-
-	import stars from "$lib/data/stars-data.json";
-
-	let width = 600;
-	let height = 600;
-
-	$: xScale = d3.scaleLinear().range([0, width]);
-
-	$: yScale = d3.scaleLinear().range([0, height]);
+	import StarsPlot from "$lib/components/stars-plot.svelte";
 </script>
 
 <div class="all">
@@ -18,21 +9,9 @@
 
 	<div class="viz">
 		<h2>Aqui entra a viz</h2>
+		<StarsPlot />
 	</div>
 </div>
-
-<svg {width} {height} viewBox="-600 -600 {width * 2} {height * 2}">
-	<g class="stars">
-		{#each stars as star}
-			<circle
-				cx={xScale(star.x_proj)}
-				cy={yScale(star.y_proj)}
-				r="1"
-				fill="white"
-			/>
-		{/each}
-	</g>
-</svg>
 
 <style>
 	.all {
